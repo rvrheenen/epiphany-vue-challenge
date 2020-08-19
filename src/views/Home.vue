f07b0968be9503c952fae264c323e4f7bab21941
rvrheenen <rick.van.rheenen@gmail.com>
2020-08-19 17:16:25 +0200

    actually link to survey page on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <router-link to="/survey">START SURVEYS</router-link>
   </div>
 </template>
 
@@ -14,3 +15,21 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+
+    a {
+      background: green;
+      text-decoration: none;
+      padding: 1rem;
+      font-size: 1.2rem;
+      color: white;
+    }
+  }
+</style>
